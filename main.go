9dec339bebcc75e3160c5f73d81bf051a2488532
Garima Sharma <gasharma@pivotal.io>
2018-08-03 10:30:57 -0400

    print the number of times IP has Requested

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-	  "encoding/json"
-    "log"
-		// "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
+	"encoding/json"
+	"log"
+	// "fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-// our main function
-func main() {
-    router := mux.NewRouter()
+var times map[string]int
 
-		router.HandleFunc("/hello", getDetails).Methods("GET")
-		log.Fatal(http.ListenAndServe(":8000", router))
+func main() {
+	router := mux.NewRouter()
+	times = make(map[string]int)
+
+	router.HandleFunc("/hello", getDetails).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func getDetails(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("hellooooo 👋")
 	json.NewEncoder(w).Encode(r.RemoteAddr)
+	json.NewEncoder(w).Encode(timesRequestBy(r.RemoteAddr))
 }
 
+func timesRequestBy(remoteAddr string) int {
+	if val, ok := times[remoteAddr]; ok {
+		times[remoteAddr] = val + 1
+	} else {
+		times[remoteAddr] = 1
+	}
+	return times[remoteAddr]
+}
