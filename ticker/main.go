56821eace2a2ff8ba5cad2b8f66f5e19f7dedfcb
Garima Sharma <gasharma@pivotal.io>
2018-08-03 16:24:33 -0400

    app that is actually a hidden cron job

--- ticker/main.go
+++ ticker/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/mileusna/crontab"
 	"time"
 )
 
@@ -9,7 +10,14 @@ func periodicFunc(tick time.Time) {
 	fmt.Println("Tick at: ", tick)
 }
 func main() {
+	ctab := crontab.New()
+
+	ctab.MustAddJob("* * * * *", myFunc2)
+
 	for t := range time.NewTicker(2 * time.Second).C {
 		periodicFunc(t)
 	}
+}
+func myFunc2(){
+	println("hell yeah")
 }
